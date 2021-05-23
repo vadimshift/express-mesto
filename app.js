6b52c498fa5aec37f7b0df49086f998451cd013a
vadimshift <vadimshift@yandex.ru>
2021-05-23 18:19:39 +0500

    создал схемы БД

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 // подключаемся к серверу mongo
-gitmongoose.connect('mongodb://localhost:27017/mestodb ', {
+mongoose.connect('mongodb://localhost:27017/mestodb ', {
   useNewUrlParser: true,
   useCreateIndex: true,
     useFindAndModify: false
